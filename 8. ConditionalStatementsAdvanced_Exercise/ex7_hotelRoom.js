f292b1f6ff22a6b75e9d2c99a8042dbf4ddec680
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2022-09-06 12:17:28 +0300

    Update ex7_hotelRoom.js

--- 8. ConditionalStatementsAdvanced_Exercise/ex7_hotelRoom.js
+++ 8. ConditionalStatementsAdvanced_Exercise/ex7_hotelRoom.js
@@ -5,31 +5,69 @@ function hotelRoom(input) {
     switch (month) {
         case "May":
         case "October":
-            let priceStudio = numberOvernightStays * 50;
-            let priceApartment = numberOvernightStays * 65;
-            if (numberOvernightStays >= 7) {
-                priceStudio -= priceStudio * 0.05;
+            //let priceStudio = numberOvernightStays * 50;
+           // let priceApartment = numberOvernightStays * 65;
+           if (numberOvernightStays > 14) {
+                let priceStudio = 50 - (50 * 0.30);
+                let finalPriceStudio = priceStudio * numberOvernightStays;
+                let priceApartment = 65 * 0.90;
+                let finalPriceApartment = priceApartment * numberOvernightStays;
+               // let finalPrice = priceApartment * numberOvernightStays;
+               console.log(`Apartment: ${finalPriceApartment.toFixed(2)} lv.`);
+                console.log(`Studio: ${finalPriceStudio.toFixed(2)} lv.`);
+            } else  if (numberOvernightStays >= 7) {
+                let priceStudio = 50 - (50 * 0.05);
+                let finalPriceStudio = priceStudio * numberOvernightStays;
+                let finalPriceApartment = 65 * numberOvernightStays;
+                //let finalPrice = priceStudio * numberOvernightStays;
+                console.log(`Apartment: ${finalPriceApartment.toFixed(2)} lv.`);
+                console.log(`Studio: ${finalPriceStudio.toFixed(2)} lv.`);
 
-            } else if (numberOvernightStays > 14) {
-                priceStudio -= priceStudio * 0.30;
-                priceApartment -= priceApartment * 0.10;
-            }
+            } 
             break;
         case "June":
         case "September":
             if (numberOvernightStays > 14) {
-                priceStudio -= priceStudio * 0.20;
-                priceApartment -= priceApartment * 0.10;
+                let priceStudio = 75.20 - (75.20 * 0.20);
+                let finalPriceStudio = priceStudio * numberOvernightStays;
+                let priceApartment = 68.70  * 0.90;
+                let finalPriceApartment = priceApartment * numberOvernightStays;
+               // let finalPrice = priceApartment * numberOvernightStays;
+                console.log(`Apartment: ${finalPriceApartment.toFixed(2)} lv.`);
+                console.log(`Studio: ${finalPriceStudio.toFixed(2)} lv.`);
+            } else {
+                let finalPriceStudio = 75.20 * numberOvernightStays;
+                let finalPriceApartment = 68.70 * numberOvernightStays;
+                console.log(`Apartment: ${finalPriceApartment.toFixed(2)} lv.`);
+                console.log(`Studio: ${finalPriceStudio.toFixed(2)} lv.`);
             }
             break;
         case "July":
             if (numberOvernightStays > 14) {
-                priceApartment -= priceApartment * 0.10;
+                let finalPriceStudio = 76 * numberOvernightStays;
+                let finalpriceApartment = 77 * 0.90 * numberOvernightStays;
+                //let finalPrice = priceApartment * numberOvernightStays;
+                console.log(`Apartment: ${finalpriceApartment.toFixed(2)} lv.`);
+                console.log(`Studio: ${finalPriceStudio.toFixed(2)} lv.`);
+            } else {
+                let finalPriceStudio = 76 * numberOvernightStays;
+                let finalPriceApartment = 77 * numberOvernightStays;
+                console.log(`Apartment: ${finalPriceApartment.toFixed(2)} lv.`);
+                console.log(`Studio: ${finalPriceStudio.toFixed(2)} lv.`);
             }
             break;
         case "August":
             if (numberOvernightStays > 14) {
-                priceApartment -= priceApartment * 0.10;
+                let finalPriceStudio = 76 * numberOvernightStays;
+                let finalpriceApartment = 77  * 0.90 * numberOvernightStays;
+               // let finalPrice = priceApartment * numberOvernightStays;
+                console.log(`Apartment: ${finalpriceApartment.toFixed(2)} lv.`);
+                console.log(`Studio: ${finalPriceStudio.toFixed(2)} lv.`);
+            } else {
+                let finalPriceStudio = 76 * numberOvernightStays;
+                let finalPriceApartment = 77 * numberOvernightStays;
+                console.log(`Apartment: ${finalPriceApartment.toFixed(2)} lv.`);
+                console.log(`Studio: ${finalPriceStudio.toFixed(2)} lv.`);
             }
             break;
 
@@ -40,7 +78,3 @@ function hotelRoom(input) {
 hotelRoom(["May", "15"]);
 hotelRoom(["June", "14"]);
 hotelRoom(["August", "20"]);
-
-
-//  console.log(`Apartment: ${цена за целият престой}} lv.`);
-//  console.log(`Studio: ${цена за целият престой} lv.`);
