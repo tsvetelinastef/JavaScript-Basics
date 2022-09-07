179ef677b8c271aa2a8e9d4e7b7c26744bb1e8c2
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2022-09-07 23:34:39 +0300

    Add files via upload

---
+++ 10. For_Loop_Exercise/ex1_numbersEndingInSeven.js
@@ -0,0 +1,10 @@
+function numbersEndingInSeven() {
+    for (let i = 7; i <= 997; i++) {
+        if(i % 10 === 7){
+            console.log(i);
+        }
+        
+    }
+}
+
+numbersEndingInSeven();
