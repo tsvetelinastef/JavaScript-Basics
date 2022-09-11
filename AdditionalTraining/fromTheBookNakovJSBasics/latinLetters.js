ebe70236a71c76c8b1c5a56e45cf31a87b899fa7
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2022-09-11 13:53:25 +0300

    Update latinLetters.js

--- AdditionalTraining/fromTheBookNakovJSBasics/latinLetters.js
+++ AdditionalTraining/fromTheBookNakovJSBasics/latinLetters.js
@@ -1,5 +1,14 @@
 function latinLetters(){
-
+    for(let i = 97 ; i <= 122; i++){
+        // from number of letter -> to just letter
+        console.log(String.fromCharCode(i));
+    }
 } 
+
+/*
+for(let ch = 'a'.charCodeAt(0); ch <= 'z'.charCodeAt(0); ch++){
+    console.log(String.fromCharCode(ch));
+}
+*/
 
 latinLetters();
