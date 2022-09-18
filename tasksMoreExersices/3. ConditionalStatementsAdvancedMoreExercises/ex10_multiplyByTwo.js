1c9c854f400598dbb235005f74583328b7ab10be
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2022-09-18 15:34:44 +0300

    Add files via upload

---
+++ tasksMoreExersices/3. ConditionalStatementsAdvancedMoreExercises/ex10_multiplyByTwo.js
@@ -0,0 +1,20 @@
+function multiplyByTwo(input) {
+    let index = 0;
+    let x = 0;
+
+    for (let i = 1; i > 0; i++) {
+        x = Number(input[index]);
+        index++;
+
+        if (x < 0) {
+            console.log("Negative number!");
+            break;
+        } else {
+            let result = x * 2;
+            console.log(`Result: ${result.toFixed(2)}`);
+        }
+    }
+}
+
+//multiplyByTwo(["12", "43.2144", "12.3", "543.23", "-20"]);
+multiplyByTwo(["23.43", "12.3245", "0", "65.23432", "23", "65", "-12"]);
---
+++ tasksMoreExersices/3. ConditionalStatementsAdvancedMoreExercises/ex9_numbersFromOnetoTen.js
@@ -0,0 +1,7 @@
+function numbersFromOnetoTen(input) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(i);
+    }
+}
+
+numbersFromOnetoTen();
