08b9bc8cc72cb75e9e59ac1fc52b3959e6ebea9f
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2022-09-18 16:53:32 +0300

    Update exSeven_Shopping.js

--- 4. ConditionalStatements-Exercise/exSeven_Shopping.js
+++ 4. ConditionalStatements-Exercise/exSeven_Shopping.js
@@ -1,31 +1,34 @@
 function shopping(input){
+
+    // !!!!!  100 out of 100   !!!!!
     let budget = Number(input[0]);
-    let videoCards = Number(input[1]);
-    let processors = Number(input[2]);
-    let ramMemory = Number(input[3]);
+    let numberGPUs = Number(input[1]);
+    let numberCPUs = Number(input[2]);
+    let numberRAMs = Number(input[3]);
+    
+    let moneyLeft = 0;
 
+    let gpusPrice = numberGPUs * 250;
 
-    let sumVideoCards = videoCards * 250;
+    let cpusPrice = gpusPrice * 0.35;
+    let ramsPrice = gpusPrice * 0.10;
+    let ramQuantWanted = ramsPrice * numberRAMs;
 
-    let priceProcessors = 0.35 * sumVideoCards;
-    let sumProcessors = processors * priceProcessors;
-
-    let priceRamMemory = 0.10 * sumVideoCards;
-    let sumRamMemory = ramMemory * priceRamMemory;
-
-    let allSum = sumVideoCards + sumProcessors + sumRamMemory;
-
-    if(videoCards >= processors){
-        let finalSum = allSum - (allSum * 0.15);
-
-        let moneyLeft = Math.abs(budget - finalSum);
-        if(finalSum <= budget){
-            console.log(`You have ${moneyLeft.toFixed(2)} leva left!`);
-        } else if (finalSum > budget) {
-            console.log(`Not enough money! You need ${moneyLeft.toFixed(2)} leva more!`);
-        }
+    let total = gpusPrice + cpusPrice * numberCPUs + ramQuantWanted;
+    
+    if (numberGPUs >= numberCPUs) {
+        total = total * 0.85;   
     }
 
+    moneyLeft = Math.abs(budget - total);
+    if (total <= budget) {
+        console.log(`You have ${moneyLeft.toFixed(2)} leva left!`);
+    } else {
+         moneyLeft = Math.abs(budget - total);
+        console.log(`Not enough money! You need ${(total - budget).toFixed(2)} leva more!`);
+    }
+    
 }
+
 shopping(["900", "2", "1", "3"]);
 shopping(["920.45", "3", "1", "1"]);
