e066311f1f6b886d87a6f7769f053985374a3e04
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2022-09-19 21:12:24 +0300

    Update ex7_sumOfNumbers.js

--- 9. For_loop_LAB/ex7_sumOfNumbers.js
+++ 9. For_loop_LAB/ex7_sumOfNumbers.js
@@ -1,15 +1,16 @@
-function sum(input) {
+function sumOfNumbers(input) {
+    let textNumber = String(input[0]);
     let sum = 0;
-    while (input != 0) {
-        sum = sum + input % 10;
-        input = parseInt(input / 10);
+  
+    for (let i = 0; i < textNumber.length; i++) {
+      let num = Number(textNumber.charAt(i));
+      sum += num;
     }
-
     console.log(`The sum of the digits is:${sum}`);
-}
+  }
+ 
 
-//sum(["1234"]);
-sum(["564891"]);
+sumOfNumbers(["1234"]);
+sumOfNumbers(["564891"]);
 
-
-// 80 out of 100
+// 100 out of 100
