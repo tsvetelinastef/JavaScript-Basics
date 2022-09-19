d8b40819051b5be4220d31c41205ef0a9cc951c3
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2022-09-19 16:07:22 +0300

    Update ex2_password.js

--- 11. While_Loop_LAB/ex2_password.js
+++ 11. While_Loop_LAB/ex2_password.js
@@ -2,8 +2,12 @@ function password(input) {
     let username = input[0];
     let password = input[1];
 
+    // variable data holds entered password  
+    // Инициализирайте променлива data, която ще държи въведената от потребителя парола за вход
     let data = input[2];
     // we've already took first three elements, so counter = 3 
+    // Инициализирайте променлива counter, която ще държи индекса на текущия елемент в масива. Тъй като вече сме присвоили
+    //първите три елемента, ще сложим counter да е равно на 3.
     let index = 3;
     // in the while loop until entering valid password, it reads new
     // and  the counter + 1
