02998352874cd25fb6d7d02230db3ad73c54fbfd
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2022-10-08 20:01:52 +0300

    Add files via upload

---
+++ 13. NestedLoops_Lab/ex1_clock.js
@@ -0,0 +1,14 @@
+function clock(input){
+    
+    for (let hours = 0; hours < 24; hours++) {
+        for (let minutes = 0; minutes < array.length; minutes++) {
+           // console.log(`${hours}:${minutes}`);
+            if (minutes < 10) {
+                console.log();
+            }
+        }
+        
+    }
+} 
+
+clock();
