9b477a203dbf1d3e52c7cc82c68b298e25f2bdb6
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2022-10-09 23:04:18 +0300

    Update ex1_clock.js

--- 13. NestedLoops_Lab/ex1_clock.js
+++ 13. NestedLoops_Lab/ex1_clock.js
@@ -1,11 +1,17 @@
-function clock(input){
+/* Write function about hours from 0:0 to 23:59 every on other row
+hours should be in this format: "{hour}:{minutes}"
+*/
+
+function clock(){
     
-    for (let hours = 0; hours < 24; hours++) {
-        for (let minutes = 0; minutes < array.length; minutes++) {
+    for (let hours = 0; hours <= 23; hours++) {
+        for (let minutes = 0; minutes <= 59; minutes++) {
            // console.log(`${hours}:${minutes}`);
-            if (minutes < 10) {
-                console.log();
-            }
+            //if (minutes < 10) {
+               // console.log(`${hours}:0${minutes}`);
+           // } else {
+                console.log(`${hours}:${minutes}`);
+            //}
         }
         
     }
