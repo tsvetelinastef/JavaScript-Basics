005c95e37f7d57add887446d8a90075ed47bce97
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2022-10-13 10:31:20 +0300

    Add files via upload

---
+++ 14. NestedLoops_Exercise/ex1_NumberPyramid.js
@@ -0,0 +1,15 @@
+function numberPyramids(input){
+    let n = Number(input[0]);
+
+    for (let rows = 1; rows <= n; rows++) {
+        for (let cols = 1; cols <= n; cols++) {
+            const element = array[cols];
+            
+        }
+        
+    }
+}
+
+numberPyramids(["7"]);
+numberPyramids(["12"]);
+numberPyramids(["15"]);
