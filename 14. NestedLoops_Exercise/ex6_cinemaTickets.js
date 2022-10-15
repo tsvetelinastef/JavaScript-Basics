8e7eece44a27035ed1515109a34d28e6addff8ec
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2022-10-16 01:05:40 +0300

    Update ex6_cinemaTickets.js

--- 14. NestedLoops_Exercise/ex6_cinemaTickets.js
+++ 14. NestedLoops_Exercise/ex6_cinemaTickets.js
@@ -76,11 +76,40 @@ function cinemaTickets(input) {
                 case "student":
                     studentCount++;
                     break;
-case 
+                case "standard":
+                    standardCount++;
+                    break;
+                case "kid":
+                    kidCount++;
+                    break;
             }
+
+            if(freeSpace === soldTicket){
+                break;
+            }
+
+            tempCommand = input[index];
+            index++;
         }
+
+        totalTicket += soldTicket;
+        let percentage = soldTicket / freeSpace * 100;
+        console.log(`${name} - ${percentage.toFixed(2)}% full.`);
+
+        command = input[index];
+        index++;
     }
+
+    console.log(`Total tickets: ${totalTicket}`);
+    let percentageStudentTicket = studentCount / totalTicket * 100;
+    let percentageStandardTicket = standardCount / totalTicket * 100;
+    let percentageKidTicket = kidCount / totalTicket * 100;
+
+    console.log(`${percentageStudentTicket.toFixed(2)}% student tickets.`);
+    console.log(`${percentageStandardTicket.toFixed(2)}% standard tickets.`);
+    console.log(`${percentageKidTicket.toFixed(2)}% kids tickets.`);
+
 }
 
 cinemaTickets(["Taxi", "10", "standard", "kid", "student", "student", "standard", "standard", "End", "Scary Movie", "6", "student", "student", "student", "student", "student", "student", "Finish"]);
-cinemaTickets(["The Matrix", "20", "student", "standard", "kid", "kid", "student", "student", "standard", "student", "End", "The Green Mile", "17", "student", "standard", "standard", "student", "standard", "student", "End", "Amadeus", "3", "standard", "standard", "standard", "Finish"]); 
+//cinemaTickets(["The Matrix", "20", "student", "standard", "kid", "kid", "student", "student", "standard", "student", "End", "The Green Mile", "17", "student", "standard", "standard", "student", "standard", "student", "End", "Amadeus", "3", "standard", "standard", "standard", "Finish"]); 
