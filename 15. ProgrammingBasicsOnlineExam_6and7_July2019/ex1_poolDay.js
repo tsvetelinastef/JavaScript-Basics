2611b1a586f1f4e900876f57a9bd2052d8f55c70
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2022-10-16 19:03:31 +0300

    Update ex1_poolDay.js

--- 15. ProgrammingBasicsOnlineExam_6and7_July2019/ex1_poolDay.js
+++ 15. ProgrammingBasicsOnlineExam_6and7_July2019/ex1_poolDay.js
@@ -1,5 +1,26 @@
-function poolDay(){
+/* Преподавателският екип на СофтУни организира работен ден на 
+басейн по случай настъпването на лятото. Вашата задача е да 
+напишете програма, която да изчислява каква сума трябва да се 
+заплати. За всеки един човек от екипа трябва да се заплати 
+такса вход. Трябва да имате предвид, че един чадър стига за двама 
+души. Знае се, че само 75% от екипа искат шезлонги. 
+При изчислението на броя на чадърите и шезлонгите, техният брой 
+да се закръгли до по-голямото цяло число. */
 
+function poolDay(input){
+    let numberPeople = Number(input[0]);
+    let entryTax = Number(input[1]);
+    let priceSunLounger = Number(input[2]);
+    let priceUmbrella = Number(input[3]);
+
+    let priceEnter = numberPeople * entryTax;
+    let sunlounger = Math.ceil(0.75 * numberPeople);
+    let taxSunLounger = sunlounger * priceSunLounger;
+    let umbrella = Math.ceil(0.50 * numberPeople);
+    let Umbrellatax = umbrella * priceUmbrella;
+
+    let total = priceEnter + taxSunLounger + Umbrellatax;
+    console.log(`${total.toFixed(2)} lv.`);
 
 }
 
