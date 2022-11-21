2e38a9bf7c513eac686d9111e71c9feb6734845a
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2022-11-21 22:43:17 +0200

    Add files via upload

---
+++ JS codes Training/First Steps in Coding - More Exercises/ex8_circleAreaPerimeter.js
@@ -0,0 +1,13 @@
+function circleAreaPerimeter(input) {
+    let r = Number(input[0]);
+    let area = Math.PI * r * r;
+    let perimeter = 2 * Math.PI * r;
+
+    console.log(`${area.toFixed(2)}`);
+    console.log(`${perimeter.toFixed(2)}`);
+}
+
+circleAreaPerimeter(["3"]);
+circleAreaPerimeter(["4.5"]);
+
+
