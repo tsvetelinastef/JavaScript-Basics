e02d554225bd0d77afacd7cdc1650f20edd9a457
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2022-11-27 11:48:18 +0200

    Update ex3_harvest.js

--- JS codes Training/Conditional Statements - More Exercises/ex3_harvest.js
+++ JS codes Training/Conditional Statements - More Exercises/ex3_harvest.js
@@ -7,34 +7,20 @@ function harvest(input) {
     let allgrape = x * y;
     let wine = Math.floor(0.40 * allgrape / 2.5);
 
-    if (wine <= z) {
-        let neededWine = Math.round(Math.abs(z - wine));
-        //let winePerOnePerson = Math.floor(neededWine / workers);
-        console.log(`It will be a tough winter! More ${neededWine} liters wine needed.`);
-    } else {
-        let wineLeft = Math.abs(z - wine);
-        let winePerOnePerson = wineLeft / workers;
+    if (wine >= z) {
+        let wineLeft = Math.ceil(wine - z);
+        let winePerOnePerson = Math.ceil(wineLeft / workers);
         console.log(`Good harvest this year! Total wine: ${wine} liters.`);
         console.log(`${wineLeft} liters left -> ${winePerOnePerson} liters per person.`)
-    }
+   
+        } else {
+            let neededWine = 0.40 * allgrape/ 2.5;
+        let neeeded = Math.floor(z - neededWine);
+        console.log(`It will be a tough winter! More ${neeeded} liters wine needed.`);
+         }
 }
 
 harvest(["650", "2", "175", "3 "]);
 harvest(["1020", "1.5", "425", "4"]);
 
-
-/*
-let grape = xSqmVineyard * ySqmGrape;
-    let wine = Math.floor(0.40 * grape / 2.5);
-
-
-    if (wine >= zLittersWine) {
-        let remain = Math.ceil(wine - zLittersWine);
-        let perPerson = Math.ceil(remain / workers);
-        console.log(`Good harvest this year! Total wine: ${wine} liters.`);
-        console.log(`${remain} liters left -> ${perPerson} liters per person.`);
-    } else {
-        let neeededWine = 0.40 * grape / 2.5;
-        let neeeded = Math.floor(zLittersWine - neeededWine);
-        console.log(`It will be a tough winter! More ${neeeded} liters wine needed.`);
-*/
+        
