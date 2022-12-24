0203c180801c24703393301c67f4c246f02e2092
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2022-12-24 11:18:41 +0200

    Add files via upload

---
+++ JS codes Training/whileLoopMoreExercises/ex4_NumbersDividedBy3WithoutReminder.js
@@ -0,0 +1,9 @@
+function numbersDividedBy3WithoutReminder(input) {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0) {
+            console.log(i);
+        }
+    }
+}
+
+numbersDividedBy3WithoutReminder([""]);
