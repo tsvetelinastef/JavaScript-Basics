117956f00e7d169907aa81d8b2ac59dd56ab6cbf
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2022-12-26 19:36:46 +0200

    Add files via upload

---
+++ JS codes Training/ForLoop_MoreExercises/ex1_backToThePast.js
@@ -0,0 +1,11 @@
+function backToThePast(input){
+    let index = 0;
+    let inheritedMoney = Number(input[index++]);
+    let year = Number(input[index++]);
+
+    
+
+}
+
+backToThePast(["50000", "1802"]);
+backToThePast(["100000", "15", "1808"]);
