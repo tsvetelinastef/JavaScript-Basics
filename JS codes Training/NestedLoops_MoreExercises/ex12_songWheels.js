162387b9bdffbe6196eab5a4bfb35b8f1a166748
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-01-04 00:34:09 +0200

    Add files via upload

---
+++ JS codes Training/NestedLoops_MoreExercises/ex12_songWheels.js
@@ -0,0 +1,48 @@
+function songWheels(input) {
+    let controlNumber = Number(input[0]);
+
+    let digitA = 0;
+    let digitB = 0;
+    let digitC = 0;
+    let digitD = 0;
+    let res = "";
+    let isFound = false;
+    let pass = "";
+    let counter = 0;
+
+
+    for (let i = 1; i <= 9; i++) {
+        for (let j = 1; j <= 9; j++) {
+            for (let k = 1; k <= 9; k++) {
+                for (let l = 1; l <= 9; l++) {
+                    digitA = i;
+                    digitB = j;
+                    digitC = k;
+                    digitD = l;
+
+                    if (digitA < digitB && digitC > digitD && digitA * digitB + digitC * digitD === controlNumber) {
+                        res += `${digitA}${digitB}${digitC}${digitD} `;
+                        counter++;
+                        if (counter === 4) {
+                            isFound = true;
+                            pass = `${digitA}${digitB}${digitC}${digitD}`;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    if (isFound) {
+        console.log(res);
+        console.log(`Password: ${pass}`);
+    } else {
+        console.log(res);
+        console.log("No!");
+    }
+}
+
+songWheels(["11"]);
+songWheels(["139"]);
+songWheels(["110"]);
+songWheels(["55"]);
